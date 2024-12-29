39140991ecb9aab4fac949a9e5a854a42f1765be
Artem <akudinov04@mail.ru>
2024-12-29 20:15:46 +0300

    fix err

--- pkg/db/migrationDatabase.go
+++ pkg/db/migrationDatabase.go
@@ -8,9 +8,9 @@ import (
 
 func MigrationDatabase() {
 	if err := DB.AutoMigrate(&models.Product{}); err != nil {
-		log.Fatalf("ошибка миграции таблицы Product: %w", err)
+		log.Fatalf("ошибка миграции таблицы Product: %v", err)
 	}
 	if err := DB.AutoMigrate(&models.User{}); err != nil {
-		log.Fatalf("ошибка миграции таблицы User: %w", err)
+		log.Fatalf("ошибка миграции таблицы User: %v", err)
 	}
 }
--- pkg/models/category.go
+++ pkg/models/category.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Category struct {
 	gorm.Model
-	ID       uint      `gorm:"id" gorm:"primaryKey;autoIncrement"`
+	ID       uint      `gorm:"primaryKey;autoIncrement"`
 	Name     string    `gorm:"name"`
 	Products []Product `gorm:"foreignKey:CategoryID"`
 }
